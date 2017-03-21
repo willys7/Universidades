fd8dede5c92a1c45c48400bf8b7bba7e6a6d4ebe
William Funtes <William Funtes>
2017-03-21 16:04:35 -0600

    fixed EmbeddedModelField

--- universidad/dashboard/models.py
+++ universidad/dashboard/models.py
@@ -4,18 +4,26 @@ from django.db import models
 
 from djangotoolbox.fields import EmbeddedModelField
 from djangotoolbox.fields import ListField
+from .forms import ObjectListField
 
 # Create your models here.
+class EmbedOverrideField(EmbeddedModelField):
+    def formfield(self, **kwargs):
+        return models.Field.formfield(self, ObjectListField, **kwargs)
+
+class Alumno(models.Model):
+	cursos = EmbedOverrideField('Curso')
+	nombre = models.CharField(max_length=255)
+	carnet = models.IntegerField()
+	edad = models.IntegerField()
+	carrera = models.CharField(max_length=255)
+	def __unicode__(self):
+		return '%s %s %s %s (%s)' % (self.cursos.nombre, self.cursos.numero_alumnos, self.cursos.fecha, self.cursos.codigo, self.nombre, self.carnet, self.edad, self.carrera)
+
 class Curso(models.Model):
     nombre = models.CharField(max_length=255)
     numero_alumnos = models.IntegerField()
     fecha = models.DateTimeField(auto_now_add=True)
-    codigo = models.CharField(max_length=255)
-
-class Alumno(models.Model):
-	nombre = models.CharField(max_length=255)
-	carnet = models.IntegerField()
-	edad = models.IntegerField()
-	cursos = ListField(EmbeddedModelField('Curso'))
-	carrera = models.CharField(max_length=255)
-
+    codigo = models.CharField(max_length=255, unique=True)
+    def __unicode__(self):
+		return '%s %s %s %s' % (self.nombre, self.numero_alumnos, self.fecha, self.codigo)
